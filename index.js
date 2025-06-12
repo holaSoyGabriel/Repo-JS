52567250e46f2509ea0ec275ffbfefda98170264
holaSoyGabriel <gabriel.hd1206@gmail.com>
2025-06-12 16:40:48 -0400

    redireccion corregida

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			spinnerContainer.classList.add("active");
 
 			setTimeout(() => {
-				window.location.href = "../registro-pregunta.html";
+				window.location.href = "../pages/registro-pregunta.html";
 			}, 2000);
 		});
 	}
